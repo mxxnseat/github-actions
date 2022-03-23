23244d316f6971c930bb1b90ca8280780a81e327
Shakrun Kirill <k.shkarun@seedium.io>
2022-03-23 15:43:03 +0200

    tt

--- test/main.spec.js
+++ test/main.spec.js
@@ -7,40 +7,46 @@ const { readJsonFile } = require("./utils");
 
 const expect = chai.expect;
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
-describe("Routes test suites", ()=>{
-    let app;
+describe("Routes test suites", () => {
+  let app;
 
-    before(async()=>{
-        app = boot();
+  before(async () => {
+    app = boot();
 
-        app.listen(3000);
+    app.listen(3000);
+  });
+
+  it("should get value from secrets in repositroy", async () => {
+    const result = process.env.test;
+
+    expect(result).to.equal("test_value");
+  });
+
+  it("should return hello world", async () => {
+    const result = await chai.request(app).get("/");
+    expect(result).status(200);
+    expect(result.body).to.equal("hello world");
+  });
+
+  it("should return hello test", async () => {
+    const result = await chai.request(app).get("/test");
+    expect(result).status(200);
+    expect(result.body).to.equal("hello test");
+  });
+
+  it("should write to file", async () => {
+    const stub = { a: 1, b: 2, filename: "test.json" };
+
+    const result = await chai.request(app).post("/").send(stub);
+    expect(result).status(201);
+
+    const dataFromFile = readJsonFile(path.resolve(__dirname, "../test.json"));
+
+    Object.keys(dataFromFile).forEach((k) => {
+      expect(dataFromFile).to.have.property(k);
+      expect(dataFromFile[k]).to.equal(stub[k]);
     });
-
-    it("should return hello world", async ()=>{
-        const result = await chai.request(app).get("/");
-        expect(result).status(200);
-        expect(result.body).to.equal("hello world");
-    });
-
-    it("should return hello test", async ()=>{
-        const result = await chai.request(app).get("/test");
-        expect(result).status(200);
-        expect(result.body).to.equal("hello test");
-    });
-
-    it("should write to file", async ()=>{
-        const stub = {a: 1, b: 2, filename: "test.json"};
-
-        const result = await chai.request(app).post("/").send(stub);
-        expect(result).status(201);
-        
-        const dataFromFile = readJsonFile(path.resolve(__dirname, "../test.json"))
-
-        Object.keys(dataFromFile).forEach((k)=>{
-            expect(dataFromFile).to.have.property(k);
-            expect(dataFromFile[k]).to.equal(stub[k]);
-        })
-    });
-})
+  });
+});
